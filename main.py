90c653f36c1287f46bd0c47dc117f9feffbed68d
JuHyeonTae <jabel123@naver.com>
2022-06-04 21:33:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ for idx, feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
         break
     else:
-        feed_date = feed_obj['published']
+        feed_date = feed['published_parsed']
         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
 
 f = open("README.md",mode="w", encoding="utf-8")
